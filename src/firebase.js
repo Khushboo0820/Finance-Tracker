08f04a064c1fde6c04a071ab9d5ae1ad5cd53ba4
unknown <khushboo.parate74@gmail.com>
2025-05-26 16:42:34 +0530

    Firebase Added

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,26 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDGb2259t3AccbzQAeJtR8PiRtzNsk4Exk",
+  authDomain: "finance-tracker-c8c95.firebaseapp.com",
+  projectId: "finance-tracker-c8c95",
+  storageBucket: "finance-tracker-c8c95.firebasestorage.app",
+  messagingSenderId: "629743407339",
+  appId: "1:629743407339:web:618fd709f2264ed3f1aeae",
+  measurementId: "G-2GJHFYFDHJ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
